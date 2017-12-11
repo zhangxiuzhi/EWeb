354896fc50275a60fd8127abe512ca05c781a746
王震甲 <wangzhenjia@mysteelsoft.com.cn>
2017-12-11 17:15:06 +0800

    添加上传内部方法

--- src/main/resources/static/jq/ajaxfileupload.js
+++ src/main/resources/static/jq/ajaxfileupload.js
@@ -248,3 +248,16 @@ jQuery
 		return data;
 	}
 });
+
+jQuery.handleError = function( s, xhr, status, e )      {
+	// If a local callback was specified, fire it
+	if ( s.error ) {
+		s.error.call( s.context || s, xhr, status, e );
+	}
+
+	// Fire the global callback
+	if ( s.global ) {
+		(s.context ? jQuery(s.context) : jQuery.event).trigger( "ajaxError", [xhr, s, e] );
+	}
+}
+
